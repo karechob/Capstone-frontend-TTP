b93770afd4ab3a543d9c9a1c4ff8b935599565b5
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-17 12:54:45 -0400

    feat[homepage styling]: add styling for home page with background video

--- wint/src/pages/home.jsx
+++ wint/src/pages/home.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "../css/home.css";
+import video from "../pictures/home_background2.mp4";
 import "../fonts/NeueKonstantGrotesk-Book.otf";
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div class="welcome-message">
-        <h2>Welcome to WiseSpend! </h2>
-        <body>
-          <p>
-            Embark on an unforgettable journey while keeping your wallet happy.
-            At Budget Wanderlust, we understand your love for travel and the
-            importance of making the most of your hard-earned money. Whether
-            you're a penny-pincher, a thrifty explorer, or simply seeking
-            incredible adventures without breaking the bank, you've come to the
-            right place.
-          </p>
-        </body>
+    <div className="home-container">
+      <video className="background-video" autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="background-animation"></div>
+      <div className="welcome-message">
+        <p className="header">Welcome to Wint! </p>
+        <p className="description">
+          Embark on an unforgettable journey while keeping your wallet happy. At
+          Budget Wanderlust, we understand your love for travel and the
+          importance of making the most of your hard-earned money. Whether
+          you're a penny-pincher, a thrifty explorer, or simply seeking
+          incredible adventures without breaking the bank, you've come to the
+          right place.
+        </p>
       </div>
     </div>
   );
