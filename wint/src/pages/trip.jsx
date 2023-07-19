82aeb7ee7d6048fcb65b81293d91e115cf03fec2
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-19 04:02:43 -0400

    feat&style[svgs + destination]: add destination picture + weather widget

--- wint/src/pages/trip.jsx
+++ wint/src/pages/trip.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import BudgetBreakdownGraph from '../components/trips/BudgetBreakdownGraph'
-import Collaborators from '../components/trips/Collaborators'
-import "../css/trip.css"
+import React from "react";
+import BudgetBreakdownGraph from "../components/trips/BudgetBreakdownGraph";
+import Collaborators from "../components/trips/Collaborators";
+import WeatherBreakdown from "../components/trips/WeatherBreakdown";
+import "../css/trip.css";
+import imgplaceholder from "../pictures/nyc.jpg";
 
 function trip() {
   return (
     <div>
-        <h1>
-            Owner & Collaborators 
-        </h1>
-        <Collaborators/>
-        <h1>
-            Budget Breakdown
-        </h1>
-        <div className='budget-graph-container'>
-           <BudgetBreakdownGraph className='graph-budget'/> 
+      <h1>Owner & Collaborators</h1>
+      <Collaborators />
+      <h1>Budget Breakdown</h1>
+      <div className="budget-graph-container">
+        <BudgetBreakdownGraph className="graph-budget" />
+      </div>
+      <div className="weather-destination-container">
+        <div className="destination-img-container">
+          <h1>Destination</h1>
+          <img
+            className="destination-img"
+            src={imgplaceholder}
+            alt="placeholder"
+          />
+          <h2>NameOfDestination</h2>
         </div>
+        <div className="weather-container">
+          <h1>Weather</h1>
+          <WeatherBreakdown />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default trip;
---
+++ wint/src/weather-svgs/Lowsvg.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+const Lowsvg = (props) => (
+  <svg
+    id="Icons"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 32 32"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style type="text/css">
+      {
+        "\n\t.st0{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}\n\t.st1{fill:none;stroke:#000000;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}\n"
+      }
+    </style>
+    <path
+      className="st0"
+      d="M12,17.81V6c0-1.66-1.34-3-3-3S6,4.34,6,6v11.81C4.21,18.85,3,20.78,3,23c0,3.31,2.69,6,6,6s6-2.69,6-6 C15,20.78,13.79,18.85,12,17.81z"
+    />
+    <circle className="st0" cx={9} cy={23} r={3} />
+    <line className="st0" x1={9} y1={17} x2={9} y2={20} />
+    <line className="st0" x1={22} y1={7} x2={22} y2={12} />
+    <polyline className="st0" points="24,5 22,7 20,5 " />
+    <line className="st0" x1={17.67} y1={9.5} x2={22} y2={12} />
+    <polyline className="st0" points="16.94,6.77 17.67,9.5 14.94,10.23 " />
+    <line className="st0" x1={17.67} y1={14.5} x2={22} y2={12} />
+    <polyline className="st0" points="14.94,13.77 17.67,14.5 16.94,17.23 " />
+    <line className="st0" x1={22} y1={17} x2={22} y2={12} />
+    <polyline className="st0" points="20,19 22,17 24,19 " />
+    <line className="st0" x1={26.33} y1={14.5} x2={22} y2={12} />
+    <polyline className="st0" points="27.06,17.23 26.33,14.5 29.06,13.77 " />
+    <line className="st0" x1={26.33} y1={9.5} x2={22} y2={12} />
+    <polyline className="st0" points="29.06,10.23 26.33,9.5 27.06,6.77 " />
+  </svg>
+);
+export default Lowsvg;
