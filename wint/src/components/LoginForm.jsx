83f24983ad357b8da8c5779ea8aeb8382ec425ed
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-21 02:49:09 -0400

    feat[loginform]: improve logic and layout of login page

--- wint/src/components/LoginForm.jsx
+++ wint/src/components/LoginForm.jsx
@@ -1,22 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchUserThunk, loginUserThunk } from "../redux/user/user.actions";
+// LoginForm.js
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUserThunk, googleSignInThunk } from "../redux/user/user.actions";
 import { useNavigate } from "react-router-dom";
-
 import "../css/login.css";
 
 function LoginForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const [email, setEmail] = useState("");
+  const [emailOrUsername, setEmailOrUsername] = useState("");
   const [password, setPassword] = useState("");
   const [formError, setFormError] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const resetForm = () => {
+    setEmailOrUsername("");
+    setPassword("");
+    setFormError("");
+    setSubmitted(false);
+    setIsSubmitting(false);
+  };
+
+  const handleEmailOrUsernameChange = (e) => {
+    setEmailOrUsername(e.target.value);
     setFormError("");
   };
 
@@ -28,38 +36,74 @@ function LoginForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubmitted(true);
+    setIsSubmitting(true);
 
-    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    const isNotEmpty = email.trim() !== "" && password.trim() !== "";
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailOrUsername);
+    const isNotEmpty = emailOrUsername.trim() !== "" && password.trim() !== "";
 
-    if (!isValidEmail || !isNotEmpty) {
-      setFormError("Please enter a valid email and password.");
+    if (!isNotEmpty) {
+      setFormError("Please enter a valid email or username and password.");
+      setIsSubmitting(false);
       return;
     }
 
-    console.log(email);
-    console.log(password);
-
     const userData = {
-      email: email,
+      email: isValidEmail ? emailOrUsername : null,
+      username: isValidEmail ? null : emailOrUsername,
       password: password,
     };
 
-    dispatch(loginUserThunk(userData));
-    setFormError("");
-    setEmail("");
-    setPassword("");
+    if (!userData.email && !userData.username) {
+      setFormError("Please enter a valid email or username.");
+      setIsSubmitting(false);
+      return;
+    }
+
+    dispatch(loginUserThunk(userData))
+      .then(() => {
+        resetForm();
+        navigate("/user");
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          setFormError("Invalid email, username, or password.");
+        } else {
+          setFormError("An error occurred. Please try again later.");
+        }
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
   };
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      dispatch(fetchUserThunk());
-      navigate("/user");
+  // const handleGoogleSignIn = () => {
+  //   window.location.href = "http://localhost:8080/auth/google";
+  // };
+
+  const handleGoogleSignIn = () => {
+    dispatch(googleSignInThunk())
+      .then(() => {
+        resetForm();
+        navigate("/user");
+      })
+      .catch((error) => {
+        console.error("Error initiating Google Sign-In:", error);
+      });
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
     }
-  }, [isLoggedIn, dispatch, navigate]);
+  };
 
   return (
-    <div className="login-container">
+    <div
+      className="login-container"
+      tabIndex="0"
+      onFocus={() => setFormError("")}
+      onKeyDown={handleKeyDown}
+    >
       <div className="login-message">LOG IN</div>
       <div className="login-body">
         <div className="login-top"></div>
@@ -67,10 +111,10 @@ function LoginForm() {
         <div className="login-center">
           <h2>Please Sign In</h2>
           <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
+            type="text"
+            placeholder="Email or Username"
+            value={emailOrUsername}
+            onChange={handleEmailOrUsernameChange}
             className="login-input"
           />
           <input
@@ -81,8 +125,26 @@ function LoginForm() {
             className="login-input"
           />
           {submitted && formError && <p className="login-error">{formError}</p>}
-          <button type="submit" className="login-btn" onClick={handleSubmit}>
-            Log in
+          <button
+            type="submit"
+            className="login-btn"
+            onClick={handleSubmit}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Logging In..." : "Log in"}
+          </button>
+          <div className="login-separator">
+            <hr />
+            <span>OR</span>
+            <hr />
+          </div>
+
+          <button
+            className="google-login-btn"
+            onClick={handleGoogleSignIn}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Logging In..." : "Sign in with Google"}
           </button>
         </div>
       </div>
