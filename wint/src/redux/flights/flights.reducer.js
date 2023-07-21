f6fa3241b807f7a831698b6dbc2a13478fcd66f7
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-21 18:59:54 -0400

    fix[redux]: fix user logic in redux and remove console logs that are not needed from other redux actions and reducers

--- wint/src/redux/flights/flights.reducer.js
+++ wint/src/redux/flights/flights.reducer.js
@@ -1,29 +1,25 @@
 import FlightsActionTypes from "./flights.types";
 
 export const INITIAL_FLIGHT_STATE = {
-    allFlights: [],
-    singleFlight: {},
-}
+  allFlights: [],
+  singleFlight: {},
+};
 
 const flightsReducer = (state = INITIAL_FLIGHT_STATE, { type, payload }) => {
-    console.log('FLIGHTREDUCER IS HANDLING FETCH ALL FLIGHT ACTION')
-    switch (type) {
-        case FlightsActionTypes.FETCH_FLIGHTS:
-            console.log(state)
-            return { ...state, allFlights: payload };
-        case FlightsActionTypes.FETCH_SINGLE_FLIGHT:
-            console.log("this is the payload for single flights")
-            console.log(payload)
-            return { ...state, singleFlight: payload };
-        case FlightsActionTypes.DELETE_FLIGHT:
-            return { ...state, singleFlight: null };
-        case FlightsActionTypes.ADD_FLIGHT:
-            return { ...state, allFlights: [...state.allFlights, payload] };
-        case FlightsActionTypes.UPDATE_FLIGHT:
-            return {...state, singleFlight: payload};
-        default:
-            return state;
-    }
-}
+  switch (type) {
+    case FlightsActionTypes.FETCH_FLIGHTS:
+      return { ...state, allFlights: payload };
+    case FlightsActionTypes.FETCH_SINGLE_FLIGHT:
+      return { ...state, singleFlight: payload };
+    case FlightsActionTypes.DELETE_FLIGHT:
+      return { ...state, singleFlight: null };
+    case FlightsActionTypes.ADD_FLIGHT:
+      return { ...state, allFlights: [...state.allFlights, payload] };
+    case FlightsActionTypes.UPDATE_FLIGHT:
+      return { ...state, singleFlight: payload };
+    default:
+      return state;
+  }
+};
 
 export default flightsReducer;
--- wint/src/redux/user/user.actions.js
+++ wint/src/redux/user/user.actions.js
@@ -1,131 +1,106 @@
 import axios from "axios";
 import UserActionTypes from "./user.types";
 
-// Fetch user
 export const fetchUser = (userData) => ({
   type: UserActionTypes.FETCH_USER,
   payload: userData,
 });
 
-export const fetchUserThunk = () => {
-  return async (dispatch, getState) => {
-    const user = getState().user;
-    console.log("this is user", user)
-    try {
-      const response = await axios.get(`http://localhost:8080/auth/me/`, {
-          withCredentials: true,
-        });
-        dispatch(fetchUser(response.data || {}))
-    } catch (error) {
-      console.error(error);
-    }
-  };
-};
-
-
-// Update user
 export const updateUser = (updateData) => ({
   type: UserActionTypes.UPDATE_USER,
   payload: updateData,
 });
 
-export const updateUserThunk = (id) => {
-  return async (dispatch) => {
-    try {
-      const response = await axios.put(`http://localhost:8080/api/me`);
-      dispatch(updateUser(response.data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-};
-
-// Signup user
 export const signupUser = (userData) => ({
   type: UserActionTypes.SIGNUP_USER,
   payload: userData,
 });
 
-export const signupUserThunk = (userData) => {
-  return async (dispatch) => {
-    try {
-      console.log("signupThunk is firing up");
-      const response = await axios.post(
-        `http://localhost:8080/auth/signup`,
-        userData,
-        {
-          withCredentials: true,
-        }
-      );
-      dispatch(signupUser(response.data));
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-};
-
-// Login user
 export const loginUser = (userData) => ({
   type: UserActionTypes.LOGIN_USER,
   payload: userData,
 });
 
-export const loginUserThunk = (userData) => {
-  return async (dispatch) => {
-    try {
-      const response = await axios.post(
-        `http://localhost:8080/auth/login`,
-        userData,
-        {
-          withCredentials: true,
-        }
-      );
-      dispatch(loginUser(response.data));
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-};
-
-// Login user with Google
-export const googleSignIn = (userData) => ({
+export const loginGoogle = (userData) => ({
   type: UserActionTypes.LOGIN_GOOGLE,
   payload: userData,
 });
 
-export const googleSignInThunk = () => {
-  return async (dispatch) => {
-    try {
-      console.log("googleSignInThunk Is Firing Up");
-      const response = await axios.get(`http://localhost:8080/auth/google`, {
-        withCredentials: true,
-      });
-      console.log("this is the response ", response)
-      dispatch(googleSignIn(response.data));
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-};
-
-// Logout user
 export const logoutUser = () => ({
   type: UserActionTypes.LOGOUT_USER,
 });
 
-export const logoutUserThunk = () => {
-  return async (dispatch) => {
-    try {
-      console.log("I am in userLogoutThunk");
-      await axios.get(`http://localhost:8080/auth/logout`, {
-        withCredentials: true,
-      });
-      dispatch(logoutUser());
-    } catch (error) {
-      console.error(error);
-    }
-  };
+export const setLoginStatus = (isLoggedIn) => ({
+  type: UserActionTypes.SET_LOGIN_STATUS,
+  payload: isLoggedIn,
+});
+
+export const fetchUserThunk = () => async (dispatch) => {
+  try {
+    console.log("FETCHUSERTHUNK FIRING UP");
+    const response = await axios.get(`http://localhost:8080/auth/me/`, {
+      withCredentials: true,
+    });
+    dispatch(fetchUser(response.data || {}));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateUserThunk = (userData) => async (dispatch) => {
+  try {
+    console.log("UPDATEUSERTHUNK FIRING UP");
+    const response = await axios.put(`http://localhost:8080/api/me`, userData);
+    dispatch(updateUser(response.data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const signupUserThunk = (userData) => async (dispatch) => {
+  try {
+    console.log("SIGNUPUSERTHUNK FIRING UP");
+
+    const response = await axios.post(
+      `http://localhost:8080/auth/signup`,
+      userData,
+      { withCredentials: true }
+    );
+    dispatch(signupUser(response.data));
+    sessionStorage.setItem("isLoggedIn", "true");
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const loginUserThunk = (userData) => async (dispatch) => {
+  try {
+    console.log("LOGINUSERTHUNK FIRING UP");
+
+    const response = await axios.post(
+      `http://localhost:8080/auth/login`,
+      userData,
+      { withCredentials: true }
+    );
+    dispatch(loginUser(response.data));
+    sessionStorage.setItem("isLoggedIn", "true");
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const logoutUserThunk = () => async (dispatch) => {
+  try {
+    console.log("LOGOUTUSERTHUNK FIRING UP");
+
+    await axios.get(`http://localhost:8080/auth/logout`, {
+      withCredentials: true,
+    });
+    dispatch(logoutUser());
+    sessionStorage.removeItem("isLoggedIn");
+  } catch (error) {
+    console.error(error);
+  }
 };
