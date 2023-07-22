9f1f16d7b5350ed4f712e301fd2fb7ab95c3af4f
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-22 04:55:51 -0400

    feat[create-new-pages]: create new pages for userProfile, userSettings, and newTrip

---
+++ wint/src/pages/newTrip.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NewTripForm from "../components/NewTripForm";
+import "../fonts/NeueKonstantGrotesk-Book.otf";
+
+function newTrip() {
+  return (
+    <div>
+      <NewTripForm />
+    </div>
+  );
+}
+
+export default newTrip;
---
+++ wint/src/pages/userSettings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Settings from "../components/Settings";
+import "../fonts/NeueKonstantGrotesk-Book.otf";
+
+function userSettings() {
+  return (
+    <div>
+      <Settings />
+    </div>
+  );
+}
+
+export default userSettings;
