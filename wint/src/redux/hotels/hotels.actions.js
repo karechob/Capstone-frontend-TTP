2de08219949c1f8b532445440d9c75f0300152d5
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-22 15:06:26 -0400

    feat[newtrips page]: create new trips form with axios call to hotel api call

--- wint/src/redux/hotels/hotels.actions.js
+++ wint/src/redux/hotels/hotels.actions.js
@@ -1,53 +1,47 @@
-//redux calls here
 import axios from "axios";
 import HotelsActionTypes from "./hotels.types";
 
-export const fetchAllHotels = (payload) => {
-    return {
-      type: HotelsActionTypes.FETCH_HOTELS,
-      payload: payload,
-    };
+export const fetchAllHotelsSuccess = (payload) => {
+  return {
+    type: HotelsActionTypes.FETCH_HOTELS,
+    payload: payload,
   };
+};
 
-  export const fetchAllHotelsThunk = () => {
-    return async (dispatch) => {
-      try {
-        const options = {
-            method: "GET",
-            url: "https://booking-com.p.rapidapi.com/v2/hotels/search-filters",
-            params: {
-              units: "metric",
-              order_by: "popularity",
-              dest_type: "city",
-              filter_by_currency: "AED",
-              adults_number: "2",
-              checkout_date: "2023-09-28",
-              dest_id: "-553173",
-              room_number: "1",
-              locale: "en-gb",
-              checkin_date: "2023-09-27",
-              children_ages: "5,0",
-              children_number: "2",
-              page_number: "0",
-              categories_filter_ids: "class::2,class::4,free_cancellation::1",
-              include_adjacency: "true"
-            },
-            headers: {
-              "X-RapidAPI-Key": process.env.HOTEL_API_KEY,
-              "X-RapidAPI-Host": process.env.FLIGHT_API_HOST
-            }
-          };
-  
-        const response = await axios(options);
-  
-        dispatch({
-          type: HotelsActionTypes.FETCH_HOTELS,
-          payload: response.data,
-        });
-      } catch (error) {
-        dispatch({
-          payload: error.message,
-        });
-      }
+export const fetchAllHotelsThunk = () => {
+  return async (dispatch) => {
+    const options = {
+      method: "GET",
+      url: "https://booking-com.p.rapidapi.com/v2/hotels/search",
+      params: {
+        order_by: "popularity",
+        adults_number: "2",
+        checkin_date: "2023-09-27",
+        filter_by_currency: "AED",
+        dest_id: "-553173",
+        locale: "en-gb",
+        checkout_date: "2023-09-28",
+        units: "metric",
+        room_number: "1",
+        dest_type: "city",
+        include_adjacency: "true",
+        children_number: "2",
+        page_number: "0",
+        children_ages: "5,0",
+        categories_filter_ids: "class::2,class::4,free_cancellation::1",
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_HOTEL_API_KEY,
+        "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+      },
     };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data)
+      dispatch(fetchAllHotelsSuccess(response.data));
+    } catch (error) {
+      console.log(error)
+    }
   };
+};
