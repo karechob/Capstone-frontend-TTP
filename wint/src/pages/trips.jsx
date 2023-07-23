b733225e3fb91634e4deb7e6a376b5a8e63e4d53
Hesham <heshamsharafaldin@gmail.com>
2023-07-23 00:50:36 -0400

    fix: trips reducer and root reducer now functional and fetchAllTripsThunk

--- wint/src/pages/trips.jsx
+++ wint/src/pages/trips.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Dispatch } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllTripsThunk } from "../redux/trips/trips.actions";
 
 function Trips() {
+ 
+  
+    const trips = useSelector((state) => state.trips.allTrips);
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(fetchAllTripsThunk());
+    }, [dispatch]);
+  
+    // Log the updated value of trips whenever it changes
+    useEffect(() => {
+      console.log("trips: ", trips);
+    }, [trips]);
+  
+  
+
+  // dispatch(fetchTripThunk(4))
   return (
     // page that displays the trips when clicking on trips on user
     <>
@@ -9,6 +29,7 @@ function Trips() {
       <button>Settings</button>
       <hr></hr>
       <h3>Previous Trips:</h3>
+      
     </>
   );
 }
