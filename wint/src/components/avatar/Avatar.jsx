f0d64f0b50fd00212c0d6e301288602b23d64d82
eluzung <luzungelaine@gmail.com>
2023-07-27 03:16:16 -0400

    feat(avatar): Add Avatar component

---
+++ wint/src/components/avatar/Avatar.jsx
@@ -0,0 +1,29 @@
+import "./Avatar.css";
+import React from "react";
+
+const Avatar = () => {
+  const avatarArray = [
+    "https://i.imgur.com/TF9TEjr.png",
+    "https://i.imgur.com/dpMYaXu.png",
+    "https://i.imgur.com/Y412yO8.png",
+    "https://i.imgur.com/7P3Ncsk.png",
+    "https://i.imgur.com/ky6Fjc0.png",
+    "https://i.imgur.com/WnjTXAa.png",
+    "https://i.imgur.com/BWasaAA.png",
+    "https://i.imgur.com/8laaP4z.png",
+    "https://i.imgur.com/Ed34k87.png",
+    "https://i.imgur.com/xUW73LZ.jpg",
+    "https://i.imgur.com/RnzsTJw.jpg",
+    "https://i.imgur.com/oCdKdbc.jpg",
+  ];
+
+  return avatarArray.map((avatar) => {
+    return (
+      <div className="avatar-element">
+        <img src={avatar} className="avatar-image" />
+      </div>
+    );
+  });
+};
+
+export default Avatar;
