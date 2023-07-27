d2bad03ff54284078b777f27dadb80ea89dd0773
Hesham <heshamsharafaldin@gmail.com>
2023-07-27 14:21:25 -0400

    fix: added collaborators and activities list

--- wint/src/components/trips/Collaborators.jsx
+++ wint/src/components/trips/Collaborators.jsx
@@ -6,39 +6,7 @@ import defaultpic3 from "../../assets/avatars/appleDog.png";
 //function will be changed so it reads an array if users, the individual divs below are just placeholders
 function Collaborators() {
   return (
-    <div className="collaborators-container">
-      <div className="collaborator-image">
-        <img
-          className="collaborator-indiv-img"
-          src={defaultpic1}
-          alt="default-pic"
-        />
-        <h2>UserName</h2>
-        <h3>Budget: $1000</h3>
-        <button className="remove-user-btn">X</button>
-      </div>
-      <div className="collaborator-image">
-        <img
-          className="collaborator-indiv-img"
-          src={defaultpic2}
-          alt="default-pic"
-        />
-        <h2>UserName</h2>
-        <h3>Budget: $1000</h3>
-        <button className="remove-user-btn">X</button>
-      </div>
-
-      <div className="collaborator-image">
-        <img
-          className="collaborator-indiv-img"
-          src={defaultpic3}
-          alt="default-pic"
-        />
-        <h2>UserName</h2>
-        <h3>Budget: $1000</h3>
-        <button className="remove-user-btn">X</button>
-      </div>
-    </div>
+    <></>
   );
 }
 
--- wint/src/pages/trip.jsx
+++ wint/src/pages/trip.jsx
@@ -4,11 +4,16 @@ import BudgetBreakdownGraph from "../components/trips/BudgetBreakdownGraph";
 import Collaborators from "../components/trips/Collaborators";
 import WeatherBreakdown from "../components/trips/WeatherBreakdown";
 import "../css/trip.css";
+import defaultpic1 from "../assets/avatars/dogGlasses.png";
+import PlaceholderActivity from "../assets/images/little-island.jpg";
+// import defaultpic2 from "../../assets/avatars/djAvi.png";
+// import defaultpic3 from "../../assets/avatars/appleDog.png";
 
 import {
   fetchImageThunk,
   fetchTripThunk,
   fetchWeatherThunk,
+  fetchCollaboratorThunk,
 } from "../redux/trips/trips.actions";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -26,6 +31,8 @@ function Trip() {
   const endDate = trip.endDate?.split("T")[0];
   console.log("start date: ", startDate);
   console.log("end date: ", endDate);
+  console.log("trip: ", trip);
+
   useEffect(() => {
     dispatch(fetchTripThunk(tripId));
   }, []);
@@ -34,18 +41,41 @@ function Trip() {
     dispatch(fetchImageThunk(trip.destination));
   }, [trip]);
 
-  useEffect(() => {
-    dispatch(fetchWeatherThunk(trip.destination, startDate, endDate));
-  }, [])
+  // useEffect(() => {
+  //   dispatch(fetchWeatherThunk(trip.destination, startDate, endDate));
+  // }, []);
 
-  console.log("weather data: ", weatherForecast);
+  //Get the total temperature for the trip and average
+  const totalTemp =
+    weatherForecast?.reduce((sum, day) => sum + day.temp, 0) || 0;
+  const averageTemp = weatherForecast?.length
+    ? totalTemp / weatherForecast.length
+    : 0;
 
   // Render the JSX outside the useEffect hooks
   return (
     <div className="background-trip-page">
       <h1>{trip.name}</h1>
       <h2>Owner & Collaborators</h2>
-      {trip.collaborators > 0 ? <Collaborators /> : <p>No Collaborators...</p>}
+      <div className="collaborators-container">
+        {trip.collaborators.length > 0 ? (
+          trip.collaborators.map((collaborator) => {
+            return (
+              <div className="collaborator-image">
+                <img
+                  className="collaborator-indiv-img"
+                  src={defaultpic1}
+                  alt="default-pic"
+                />
+                <h2>{collaborator.username}</h2>
+                <h3>Budget: {collaborator.budget}</h3>
+              </div>
+            );
+          })
+        ) : (
+          <p>No Collaborators...</p>
+        )}
+      </div>
       <div className="weather-destination-container">
         <div className="destination-img-container">
           <h1>Destination</h1>
@@ -55,12 +85,8 @@ function Trip() {
         <div className="weather-container">
           <h1>Weather</h1>
           <WeatherBreakdown />
-          <p>Forecast:</p>
-          {weatherForecast?.map((day) => (
-            <div className="weather-forecast">
-              <p>{day.temp}</p>
-            </div>
-          ))}
+          <p>Average Temperature:</p>
+          <p>{averageTemp.toFixed(2)}°C</p>
         </div>
       </div>
       <h1>Budget Breakdown</h1>
@@ -68,7 +94,27 @@ function Trip() {
         <BudgetBreakdownGraph className="graph-budget" />
       </div>
       <h1>Activities</h1>
-      <ActivitiesList />
+      <div className="activities-list-container">
+        {trip.activities.map((activity) => {
+
+        return(
+      <div className="act-list-item">
+        <div className="activity-img-container">
+          <img
+            className="activity-img"
+            src={PlaceholderActivity}
+            alt="Activity"
+          />
+        </div>
+        <div className="activity-info">
+          <h1>ActivityName: {activity.name}</h1>
+          <h2>Budget: ${activity.cost}</h2>
+          <h2>Link to place googlemaps</h2>
+        </div>
+      </div>
+        );
+        })}
+      </div>
       <h1>Travel & Stay</h1>
       <h1>Things to Note</h1>
     </div>
