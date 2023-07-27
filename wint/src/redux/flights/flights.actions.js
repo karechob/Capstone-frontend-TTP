001f751601747a14c124399d380f677ffff4c499
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-27 04:20:25 -0400

    feat[redux/flights]: add redux actions to flight

--- wint/src/redux/flights/flights.actions.js
+++ wint/src/redux/flights/flights.actions.js
@@ -4,37 +4,109 @@ import FlightsActionTypes from "./flights.types";
 // getting random flight data no particular values yet
 // need to pass our own values for parameters (all required)
 
-export const fetchAllFlights = (payload) => {
+export const fetchAirports = (payload) => {
   return {
-    type: FlightsActionTypes.FETCH_FLIGHTS,
+    type: FlightsActionTypes.FETCH_AIRPORT_SUCCESS,
     payload: payload,
   };
 };
 
-export const fetchAllFlightsThunk = () => {
-  return async (dispatch) => {
+export const fetchItineraries = (payload) => {
+  return {
+    type: FlightsActionTypes.FETCH_ITINERARY_SUCCESS,
+    payload: payload,
+  };
+};
+
+export const fetchAirportsThunk = (name) => async (dispatch) => {
+  try {
     const options = {
       method: 'GET',
-      url: 'https://priceline-com-provider.p.rapidapi.com/v2/flight/roundTrip',
+      url: 'http://localhost:8080/api/flights/departure', 
+      params: { name: name }, 
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_X_FLIGHT_API_KEY,
+        'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com',
+      },
+    };
+
+    const response = await axios.request(options);
+    dispatch({
+      type: FlightsActionTypes.FETCH_AIRPORT_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FlightsActionTypes.FETCH_ERROR,
+      payload: error.message,
+    });
+  }
+};
+
+
+export const fetchFlights = (departureAirport, returnAirport, departureDate, returnDate) => async (dispatch) => {
+  try {
+    const departureAndReturnDates = `${departureDate},${returnDate}`;
+    const origin = `${departureAirport},${returnAirport}`;
+    const destination = `${returnAirport},${departureAirport}`;
+
+    const options = {
+      method: 'GET',
+      url: 'http://localhost:8080/api/flights/allflights', 
       params: {
-        departure_date: '2023-12-21,2023-12-25',
+        departure_date: departureAndReturnDates,
         adults: '1',
         sid: 'iSiX639',
-        origin_airport_code: 'YWG,JFK',
-        destination_airport_code: 'JFK,YWG'
+        origin_airport_code: origin,
+        destination_airport_code: destination,
+        number_of_itineraries: '4',
+        currency: 'USD',
       },
       headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_X_FLIGHT_API_KEY,
-        'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
-      }
+        'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com',
+      },
     };
-    
-    try {
-      const response = await axios.request(options);
-      console.log("this is fligths", response.data)
-      dispatch(fetchAllFlights(response.data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+
+    const response = await axios.request(options);
+    dispatch({
+      type: FlightsActionTypes.FETCH_ITINERARY_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FlightsActionTypes.FETCH_ERROR,
+      payload: error.message,
+    });
+  }
 };
+
+
+
+// export const fetchAllFlightsThunk = () => {
+//   return async (dispatch) => {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://priceline-com-provider.p.rapidapi.com/v2/flight/roundTrip',
+//       params: {
+//         departure_date: '2023-12-21,2023-12-25',
+//         adults: '1',
+//         sid: 'iSiX639',
+//         origin_airport_code: 'YWG,JFK',
+//         destination_airport_code: 'JFK,YWG'
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': process.env.REACT_APP_X_FLIGHT_API_KEY,
+//         'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+//       }
+//     };
+    
+//     try {
+//       const response = await axios.request(options);
+//       console.log("this is fligths", response.data)
+//       dispatch(fetchAllFlights(response.data));
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+// };
--- wint/src/redux/flights/flights.reducer.js
+++ wint/src/redux/flights/flights.reducer.js
@@ -1,13 +1,19 @@
 import FlightsActionTypes from "./flights.types";
 
 export const INITIAL_FLIGHT_STATE = {
-  allFlights: [],
+  airportData: {},
+  itineraryData: {},
+  error: null,
 };
 
 const flightsReducer = (state = INITIAL_FLIGHT_STATE, { type, payload }) => {
   switch (type) {
-    case FlightsActionTypes.FETCH_FLIGHTS:
-      return { ...state, allFlights: payload };
+    case FlightsActionTypes.FETCH_AIRPORT_SUCCESS:
+      return { ...state, airportData: payload, error: null };
+      case FlightsActionTypes.FETCH_INFORMATION_SUCCESS:
+        return { ...state, itineraryData: payload, error: null };
+        case FlightsActionTypes.FETCH_ERROR:
+          return { ...state, error: payload };
     default:
       return state;
   }
