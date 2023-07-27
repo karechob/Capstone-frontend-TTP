0dcca04879688bc545b22c816b3ef139da53e909
eluzung <luzungelaine@gmail.com>
2023-07-27 04:49:29 -0400

    feat(pages): Add Avatar page

---
+++ wint/src/pages/avatar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "../components/Avatar";
+import "../css/avatar.css";
+
+function avatar() {
+  return (
+    <div className="avatar-page">
+      <h1>Select an Avatar!</h1>
+      <Avatar />
+    </div>
+  );
+}
+
+export default avatar;
