ed88313dfbb8addae053a2dd4d2fed706da78668
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-28 05:50:11 -0400

    edit[formatDate2]: edit the method formatDate2 for accurate date formatting

--- wint/src/components/formatDate.jsx
+++ wint/src/components/formatDate.jsx
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export function formatDate1(dateStr) {
   const options = {
     year: "numeric",
@@ -10,11 +12,7 @@ export function formatDate1(dateStr) {
   return new Date(dateStr).toLocaleDateString(undefined, options);
 }
 
-
 export function formatDate2(dateString) {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
+  const date = moment.utc(dateString).local().toDate();
+  return moment(date).format("YYYY-MM-DD");
 }
