ff8b4fca587014067341a6ce5a675a423ae43d32
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-28 05:38:47 -0400

    edit[flights redux]: make some tweaks, improvements, and remove unneeded code

--- wint/src/redux/flights/flights.actions.js
+++ wint/src/redux/flights/flights.actions.js
@@ -1,44 +1,22 @@
 import axios from "axios";
 import FlightsActionTypes from "./flights.types";
 
-// getting random flight data no particular values yet
-// need to pass our own values for parameters (all required)
-
-export const fetchAirports = (payload) => {
-  return {
-    type: FlightsActionTypes.FETCH_AIRPORT_SUCCESS,
-    payload: payload,
-  };
-};
-
 export const fetchItineraries = (payload) => {
   return {
-    type: FlightsActionTypes.FETCH_ITINERARY_SUCCESS,
+    type: FlightsActionTypes.FETCH_ITINERARIES,
     payload: payload,
   };
 };
 
-
-export const fetchAirportsThunk = (name) => async (dispatch) => {
+export const fetchItinerariesThunk = (newTripData) => async (dispatch) => {
   try {
-    console.log("FETCH AIRPORT THUNK IS FIRING UP");
-    console.log("this is name", name);
-    const response = await axios.post(`http://localhost:8080/api/flights/departure`, {name: name}, {withCredentials : true},);
-    console.log("this is response data", response.data)
-    dispatch(fetchAirports(response.data));
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-export const fetchFlights = (departureAirport, returnAirport, departureDate, returnDate) => async (dispatch) => {
-  try {
-    const departureAndReturnDates = `${departureDate},${returnDate}`;
-    const origin = `${departureAirport},${returnAirport}`;
-    const destination = `${returnAirport},${departureAirport}`;
-    const response = await axios.post(`http://localhost:8080/api/flights/allflights`, 
-    {departure_date: departureAndReturnDates}, {origin_airport_code: origin}, {destination_airport_code: destination}, {withCredentials : true},);
+    console.log("FETCH ITINERARIES THUNK IS FIRING UP");
+    const response = await axios.post(
+      `http://localhost:8080/api/flights/allflights`,
+      newTripData,
+      { withCredentials: true }
+    );
+    console.log(response.data);
     dispatch(fetchItineraries(response.data));
   } catch (error) {
     console.error(error);
