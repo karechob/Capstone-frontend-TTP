de66b2fb61e4484c096dc24b63ed3b990b247d58
eluzung <luzungelaine@gmail.com>
2023-07-27 20:24:39 -0400

    feat: Add Activities actions to connect to backend

--- wint/src/redux/activities/activities.actions.js
+++ wint/src/redux/activities/activities.actions.js
@@ -1,9 +1,26 @@
 import axios from "axios";
 import ActivitiesActionTypes from "./activities.types";
 
-export const fetchAllFlights = (payload) => {
-  return {
-    type: FlightsActionTypes.FETCH_FLIGHTS,
-    payload: payload,
+export const fetchActivities = (payload) => ({
+  type: ActivitiesActionTypes.FETCH_ACTIVITIES,
+  payload: payload,
+});
+
+export const fetchActivitiesThunk = (destination) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/api/activities/allactivities`,
+        { destination: destination },
+        {
+          withCredentials: true,
+        }
+      );
+      
+      console.log("This is response data:", response.data);
+      dispatch(fetchActivities(response.data));
+    } catch (error) {
+      console.error(error);
+    }
   };
 };
