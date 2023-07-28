3803f4356c0b9f3898f3cc3f236f2a6ffc94a4c2
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-28 05:41:13 -0400

    edit[flights redux]: make some tweaks, improvements, and remove unneeded code

--- wint/src/redux/hotels/hotels.actions.js
+++ wint/src/redux/hotels/hotels.actions.js
@@ -1,47 +1,22 @@
 import axios from "axios";
 import HotelsActionTypes from "./hotels.types";
 
-export const fetchDestination = (payload) => {
+export const fetchHotels = (payload) => {
   return {
-    type: HotelsActionTypes.FETCH_DESTINATION,
+    type: HotelsActionTypes.FETCH_HOTELS,
     payload: payload,
   };
 };
 
-export const fetchInformation = (payload) => {
-  return {
-    type: HotelsActionTypes.FETCH_INFORMATION,
-    payload: payload,
-  };
-};
-
-
-export const fetchDestinationThunk = (name) => async (dispatch) =>{
-    try {
-      const response = await axios.post(`http://localhost:8080/api/hotels/destination`, {name: name}, {withCredentials : true},);
-      console.log("this is destination for hotels", response.data)
-      dispatch(fetchDestination(response.data));
-    } catch (error) {
-      console.log(error)
-    }
-  };
-
-// const pricerangeformat = `price::USD${lower}-${higher}`;  
-export const fetchInformationThunk = (checkout_date, dest_id, checkin_date, categories_filter_ids) => async (dispatch) => {
+export const fetchHotelsThunk = (newTripData) => async (dispatch) => {
   try {
-    console.log("FETCH AIRPORT THUNK IS FIRING UP");
-    console.log("this is checkout date", checkout_date); //(2023-08-29)
-    console.log("this is dest_id", dest_id);
-    console.log("this is checkin_date", checkin_date);
-    console.log("this is categories_filter_ids", categories_filter_ids);
-    const response = await axios.post(`http://localhost:8080/api/hotels/information`, 
-    {checkout_date: checkout_date},
-    {dest_id: dest_id},
-    {checkin_date: checkin_date},
-    {categories_filter_ids: categories_filter_ids},
-    {withCredentials : true},);
-    console.log("this is response data", response.data)
-    dispatch(fetchInformation(response.data));
+    const response = await axios.post(
+      `http://localhost:8080/api/hotels/information`,
+      newTripData,
+      { withCredentials: true }
+    );
+    console.log("this is response data", response.data);
+    dispatch(fetchHotels(response.data));
   } catch (error) {
     console.log(error);
   }
